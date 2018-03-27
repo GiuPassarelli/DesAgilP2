8d01e0f7d12e2a05ff2d6a4196093af2ed92b5a7
GiuPassarelli <gb_passarelli@hotmail.com>
2018-03-27 18:44:51 -0300

    Add files via upload

---
+++ desagil-projeto2/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/AndGate.java
@@ -0,0 +1,17 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+public class AndGate extends Gate{
+
+	@Override
+	public boolean read() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
